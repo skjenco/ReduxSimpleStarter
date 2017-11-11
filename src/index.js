3af299b13ba03b9ab68cfa712cfd23b6a70187b3
stephen.jensen <stephen.jensen@stgconsulting.com>
2017-11-11 00:45:11 -0700

    How we do it with es6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';  //How we can use React
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
+//youtube api key
+const API_KEY = 'AIzaSyANmYAtFZWVSvu2erxCcQDuijWQKnCAm3k'
 
 //Create a new compenent.  Idea hear is to produce html
 //New Component.  Code that produces html
 
 
 const App = () => {   //Often called the fat arrow in Es6 it replace the function.  "this" behaves different
-    return <div>Working with the fat arrow</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 
