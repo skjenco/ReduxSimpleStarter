9525e8c52bfab6718958972889fa002956c935c2
stephen.jensen <stephen.jensen@stgconsulting.com>
2017-11-10 19:14:47 -0700

    How we do it with es6

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ import ReactDOM from 'react-dom';
 //New Component.  Code that produces html
 
 
-//Need to think of the following as a factory (oo thinkg a class)-- it is not an instance
-const App = function() {
-    return <div>Hi there and more!</div>;
+const App = () => {   //Often called the fat arrow in Es6 it replace the function.  "this" behaves different
+    return <div>Working with the fat arrow</div>;
 }
 
 
 //Take this compents generated html and put it on the page (in the DOM)
 
-ReactDOM.render(<App />, document.querySelector('.container'));  //So why <App />  not just App.   Because App as mentioned above is my class when I <App /> we get and Instace of App
-//<App /> in Jsx creates in es5 React.createElement(App, null);  so it creates that element
-
-// The second argument is to tell where we place this new elment we created I could use ReactDOM.render(<App />, document.getElementById('rootElement'));
+ReactDOM.render(<App />, document.querySelector('.container'));
